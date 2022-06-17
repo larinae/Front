9b6bbe8aa9e55affd03526147d6ac6915319294f
larinae <63452797+larinae@users.noreply.github.com>
2022-06-17 20:32:20 +1000

    Add files via upload

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,25 @@
 const mix = require('laravel-mix');
+const { resolve } = require('path');
 
-mix
-    .setPublicPath('public')
-    .js('resources/scripts/main.js', 'build/bundle.js')
-    .sass('resources/styles/main.scss', 'build/bundle.css')
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/scripts/main.js', 'public/build')
+    .sass('resources/styles/main.scss', 'public/build')
     .browserSync({
         proxy: 'localhost',
         files: [
             'public/**/*',
-            'resources/**/*.twig',
-            'resources/**/*.php',   
+            'resources/**/*/.twig',
+            'resources/**/*/.php',
         ],
     });
+mix.alias({'uikit-util': resolve(__dirname, 'node_modules/uikit/src/js/util')});
